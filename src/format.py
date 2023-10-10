945959c3e3b3839918d9e2f6e4c4c70df7c3c860
SRaswan <shauryaraswan@gmail.com>
2023-10-09 23:10:19 -0700

    LOL

--- src/format.py
+++ src/format.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import json
+import os
 
 def load_json(file_name: str) -> pd.DataFrame:
     """Given a path to a json file, uses pandas to load the data into
     a dataframe"""
     with open(file_name) as f:
-        leagues_json = json.load(f)
+        f_json = json.load(f)
 
-    return pd.json_normalize(leagues_json)
+    return pd.json_normalize(f_json)
+
+
+if __name__ == "__main__":
+    jsonload = []
+    for filename in os.listdir("esports-data/"):
+        json.append(load_json(filename))
